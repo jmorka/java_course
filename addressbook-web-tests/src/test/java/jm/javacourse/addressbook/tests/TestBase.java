00395d5b71f6b99b9143c0225d7784bd9da246da
jmorka <jmorka87@gmail.com>
2019-02-17 16:52:08 +0100

    Add different browsers handling

--- addressbook-web-tests/src/test/java/jm/javacourse/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/jm/javacourse/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package jm.javacourse.addressbook.tests;
 
 import jm.javacourse.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
