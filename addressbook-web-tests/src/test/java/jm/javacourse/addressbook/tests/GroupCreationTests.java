3ded1efb45dd362cb068e26df58739d1b2513156
jmorka <jmorka87@gmail.com>
2019-02-24 21:41:49 +0100

    add pre-conditions for groups creation and modification

--- addressbook-web-tests/src/test/java/jm/javacourse/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/jm/javacourse/addressbook/tests/GroupCreationTests.java
@@ -9,9 +9,6 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
 
     app.getNavigationHelper().goToGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
+    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
   }
 }
--- addressbook-web-tests/src/test/java/jm/javacourse/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/jm/javacourse/addressbook/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package jm.javacourse.addressbook.tests;
 
+import jm.javacourse.addressbook.model.GroupData;
 import org.testng.annotations.Test;
 
 public class GroupDeletionTests extends TestBase {
@@ -8,6 +9,9 @@ public class GroupDeletionTests extends TestBase {
   public void testGroupDeletion() throws Exception {
 
     app.getNavigationHelper().goToGroupPage();
+    if(! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
