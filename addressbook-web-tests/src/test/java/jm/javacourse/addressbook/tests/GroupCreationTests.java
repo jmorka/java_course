ea7407ddf67789b89f54610fdf66174827b83056
jmorka <jmorka87@gmail.com>
2019-03-21 20:43:06 +0100

    Comparison of lists in group tests

--- addressbook-web-tests/src/test/java/jm/javacourse/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/jm/javacourse/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,8 @@ import jm.javacourse.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -13,8 +15,15 @@ public class GroupCreationTests extends TestBase {
 
     app.getNavigationHelper().goToGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    GroupData group = new GroupData("test1", null, null);
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+    before.add(group);
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 }
