5f8fd17944952f93f54b2d14e78f2186b32bb2a7
jmorka <jmorka87@gmail.com>
2019-03-25 23:53:46 +0100

    Hamcrest for group tests

--- addressbook-web-tests/src/test/java/jm/javacourse/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/jm/javacourse/addressbook/appmanager/GroupHelper.java
@@ -1,6 +1,7 @@
 package jm.javacourse.addressbook.appmanager;
 
 import jm.javacourse.addressbook.model.GroupData;
+import jm.javacourse.addressbook.model.Groups;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -79,8 +80,8 @@ public class GroupHelper extends HelperBase{
    return wd.findElements(By.name("selected[]")).size();
   }
 
-  public Set<GroupData> all() {
-    Set<GroupData> groups = new HashSet<>();
+  public Groups all() {
+    Groups groups = new Groups();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements){
       String name = element.getText();
