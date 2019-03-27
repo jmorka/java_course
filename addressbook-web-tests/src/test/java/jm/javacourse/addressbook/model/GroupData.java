c3a68112f89ed2b0d638d6826c47693c0001715f
jmorka <jmorka87@gmail.com>
2019-03-27 21:30:41 +0100

    Use xml to store the data

--- addressbook-web-tests/src/test/java/jm/javacourse/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/jm/javacourse/addressbook/model/GroupData.java
@@ -1,71 +1,76 @@
 package jm.javacourse.addressbook.model;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.util.Objects;
 
+@XStreamAlias("group")
 public class GroupData {
-  private int id = Integer.MAX_VALUE;
-  private String name;
-  private String header;
-  private String footer;
+    @XStreamOmitField
+    private int id = Integer.MAX_VALUE;
+    private String name;
+    private String header;
+    private String footer;
 
 
-  public int getId() {
-    return id;
-  }
+    public int getId() {
+        return id;
+    }
 
-  public GroupData withId(int id) {
-    this.id = id;
-    return this;
-  }
+    public GroupData withId(int id) {
+        this.id = id;
+        return this;
+    }
 
-  public GroupData withName(String name) {
-    this.name = name;
-    return this;
-  }
+    public GroupData withName(String name) {
+        this.name = name;
+        return this;
+    }
 
-  public GroupData withHeader(String header) {
-    this.header = header;
-    return this;
-  }
+    public GroupData withHeader(String header) {
+        this.header = header;
+        return this;
+    }
 
-  public GroupData withFooter(String footer) {
-    this.footer = footer;
-    return this;
-  }
+    public GroupData withFooter(String footer) {
+        this.footer = footer;
+        return this;
+    }
 
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "id='" + id + '\'' +
-            ", name='" + name + '\'' +
-            '}';
-  }
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public String getHeader() {
-    return header;
-  }
+    public String getHeader() {
+        return header;
+    }
 
-  public String getFooter() {
-    return footer;
-  }
+    public String getFooter() {
+        return footer;
+    }
 
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return id == groupData.id &&
-            Objects.equals(name, groupData.name);
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return id == groupData.id &&
+                Objects.equals(name, groupData.name);
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
 
