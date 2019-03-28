d0f64c4d970630718de24ab1d1c692a4b7291918
jmorka <jmorka87@gmail.com>
2019-03-28 18:29:35 +0100

    Receiving data from DB with hibernate

--- addressbook-web-tests/src/test/java/jm/javacourse/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/jm/javacourse/addressbook/model/GroupData.java
@@ -3,78 +3,95 @@ package jm.javacourse.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
 @XStreamAlias("group")
+@Entity
+@Table(name = "group_list")
 public class GroupData {
-    @XStreamOmitField
-    private int id = Integer.MAX_VALUE;
-    @Expose
-    private String name;
-    @Expose
-    private String header;
-    @Expose
-    private String footer;
+  @XStreamOmitField
+  @Id
+  @Column(name = "group_id")
+  private int id = Integer.MAX_VALUE;
+
+  @Expose
+  @Column(name = "group_name")
+  private String name;
+
+  @Expose
+  @Column(name = "group_header")
+  @Type(type = "text")
+  private String header;
+
+  @Expose
+  @Column(name = "group_footer")
+  @Type(type = "text")
+  private String footer;
 
 
-    public int getId() {
-        return id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public GroupData withId(int id) {
-        this.id = id;
-        return this;
-    }
+  public GroupData withId(int id) {
+    this.id = id;
+    return this;
+  }
 
-    public GroupData withName(String name) {
-        this.name = name;
-        return this;
-    }
+  public GroupData withName(String name) {
+    this.name = name;
+    return this;
+  }
 
-    public GroupData withHeader(String header) {
-        this.header = header;
-        return this;
-    }
+  public GroupData withHeader(String header) {
+    this.header = header;
+    return this;
+  }
 
-    public GroupData withFooter(String footer) {
-        this.footer = footer;
-        return this;
-    }
+  public GroupData withFooter(String footer) {
+    this.footer = footer;
+    return this;
+  }
 
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            '}';
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getHeader() {
-        return header;
-    }
+  public String getHeader() {
+    return header;
+  }
 
-    public String getFooter() {
-        return footer;
-    }
+  public String getFooter() {
+    return footer;
+  }
 
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GroupData groupData = (GroupData) o;
-        return id == groupData.id &&
-                Objects.equals(name, groupData.name);
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return id == groupData.id &&
+            Objects.equals(name, groupData.name);
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name);
+  }
 }
 
